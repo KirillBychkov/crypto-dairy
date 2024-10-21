561197924f006c0f46bfceca18ca0812a9da92e1
KirillBychkov <bychkovkirill@live.com>
2024-10-21 21:28:33 +0300

    init

--- controllers/Trade.controller.js
+++ controllers/Trade.controller.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-dotenv.config({ path: path.join(__dirname, '../', '.env') });
+dotenv.config({ path: path.join(__dirname, '..', '.env') });
 
 console.log('__dirname controller', __dirname)
 
